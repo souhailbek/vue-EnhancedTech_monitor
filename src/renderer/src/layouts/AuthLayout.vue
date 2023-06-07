da323aed4936fad99f120d74a7d4729e4d23a3c2
Souhail Bektachi <souhalshiv@gmail.com>
2023-06-07 01:06:20 +0100

    feat: implement application layouts

---
+++ src/renderer/src/layouts/AuthLayout.vue
@@ -0,0 +1,36 @@
+<script setup>
+import TitleBar from "../components/TitleBar.vue";
+import {useElementBounding} from '@vueuse/core'
+import {ref} from "vue";
+
+const el = ref(null)
+const {height} = useElementBounding(el)
+
+</script>
+
+<template>
+  <div class="container grid grid-nogutter surface-ground h-screen justify-content-center">
+    <TitleBar
+      ref="el"
+      class="col-12 "
+      hide-logo
+      hide-tabs
+    />
+    <div
+      style="width: 100%;"
+      :style="`height: calc(100% - ${ height }px)`"
+    >
+      <div
+        class="flex align-items-center justify-content-center h-full overflow-hidden"
+      >
+        <div class="flex flex-column align-items-center justify-content-center">
+          <router-view />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/renderer/src/layouts/MainLayout.vue
@@ -0,0 +1,32 @@
+<script setup>
+import TitleBar from "../components/TitleBar.vue";
+import {ref} from 'vue'
+import {useElementBounding} from '@vueuse/core'
+import {useAppStore} from "../stores/AppStore";
+
+const el = ref(null)
+const {height} = useElementBounding(el);
+const {ui} = useAppStore();
+</script>
+
+<template>
+  <div
+    class="flex flex-column surface-ground h-screen justify-content-start align-items-center min-h-screen pb-5"
+  >
+    <TitleBar
+      ref="el"
+      class="col-12 h-3rem"
+    />
+    <router-view
+      style="width: 100%;"
+      :style="`min-height: calc(100% - ${ height }px)`"
+      class="content-container pt-5 px-8"
+    />
+  </div>
+</template>
+
+<style>
+.content-container {
+  max-width: 1200px;
+}
+</style>
